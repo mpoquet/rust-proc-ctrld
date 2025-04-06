88a6011516ad2c7a50e273fbcd48d1a320293784
Lagier Hadrien <ylala2910@gmail.com>
2025-04-06 11:03:33 +0200

    (feat) modification de cargo + premier prototype du démon en rust

--- rust/src/proto/demon.rs
+++ rust/src/proto/demon.rs
@@ -1,6 +1,25 @@
-
+use tokio::io::BufReader;
+use tokio::net::TcpStream;
+use tokio::sync::mpsc;
 
 #[tokio::main]
 async fn main() {
+    let port: u16 = 8080;
+    println!("We listen on the port {}", port);
 
+    let tcp_stream = TcpStream::connect(format!("127.0.0.1:{}", port)).await.expect("error connecting tcp");
+    let buffer = BufReader::new(tcp_stream);
+
+    let (tx, mut rx) = mpsc::channel::<&str>(100);
+
+    // tokio::spawn(
+    //     // Logique de réception est d'envoi
+    // );
+
+
+    loop {
+        if let Some(msg) = rx.recv().await {
+            println!("got {}", msg);
+        }
+    }
 }
