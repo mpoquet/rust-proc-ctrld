0a3fe920b8b3c49c649a7fc49530231b9914e4ab
Maati-MYS <maati.mys@gmail.com>
2025-05-12 15:42:42 +0200

    serialisation python ajout inotify

--- tests/serialisation_python.py
+++ tests/serialisation_python.py
@@ -2,6 +2,8 @@ import flatbuffers
 import os
 import sys
 
+import demon.Surveillance
+
 sys.path.append(os.path.join(os.path.dirname(__file__), '../demon'))
 
 import demon.Event
@@ -12,10 +14,31 @@ import demon.Message
 import demon.RunCommand
 import demon.InotifyEvent
 import demon.Event
+import demon.TCPSocket
+import demon.Surveillance
+import demon.SurveillanceEvent
 
 
-def serialisation(path, args, envp, flags, stack_size):
+def serialisation():
     #Variables de RunCommand à envoyer au démon
+    path = "echo"
+    args = "bonjour test"
+    envp = "oui non"
+    flags = 0
+    stack_size = 0
+
+    #Variable TCPSocket
+    destport = 42
+
+    #Variables Inotify
+    root_paths = "root"
+    mask = 4
+    size = 5
+
+
+
+
+    #Traitement des [String]
     args = args.split()
     envp = envp.split()
 
@@ -24,6 +47,7 @@ def serialisation(path, args, envp, flags, stack_size):
     bldr = flatbuffers.Builder(1024)
 
     path_bldr = bldr.CreateString(path)
+    root_paths = bldr.CreateString(root_paths)
 
     #sérialisation des arguments
     liste = []
@@ -47,6 +71,40 @@ def serialisation(path, args, envp, flags, stack_size):
     envp_bldr = bldr.EndVector()
 
 
+    #sérialisation TCPSocket
+    demon.TCPSocket.Start(bldr)
+    demon.TCPSocket.AddDestport(bldr, destport)
+    tcp_socket = demon.TCPSocket.End(bldr)
+
+    
+    #sérialisation Inotify
+    demon.Inotify.Start(bldr)
+    demon.Inotify.AddRootPaths(bldr, root_paths)
+    demon.Inotify.AddMask(bldr, mask)
+    demon.Inotify.AddSize(bldr, size)
+    inotify = demon.Inotify.End(bldr)
+
+
+    #sérialisation surveillance1
+    demon.SurveillanceEvent.Start(bldr)
+    demon.SurveillanceEvent.AddEventType(bldr, demon.Surveillance.Surveillance().TCPSocket)
+    demon.SurveillanceEvent.AddEvent(bldr, tcp_socket)
+    surveillance1 = demon.SurveillanceEvent.End(bldr)
+
+    #sérialisation surveillance2
+    demon.SurveillanceEvent.Start(bldr)
+    demon.SurveillanceEvent.AddEventType(bldr, demon.Surveillance.Surveillance().Inotify)
+    demon.SurveillanceEvent.AddEvent(bldr, inotify)
+    surveillance2 = demon.SurveillanceEvent.End(bldr)
+
+
+    #sérialisation de toWatch
+    demon.RunCommand.StartToWatchVector(bldr, 2)
+    bldr.PrependUOffsetTRelative(surveillance1)
+    bldr.PrependUOffsetTRelative(surveillance2)
+    to_watch = bldr.EndVector()
+
+
     #Serialisation RunCommand
     demon.RunCommand.Start(bldr)
     demon.RunCommand.AddPath(bldr, path_bldr)
@@ -54,6 +112,7 @@ def serialisation(path, args, envp, flags, stack_size):
     demon.RunCommand.AddEnvp(bldr, envp_bldr)
     demon.RunCommand.AddFlags(bldr, flags)
     demon.RunCommand.AddStackSize(bldr, stack_size)
+    demon.RunCommand.AddToWatch(bldr, to_watch)
 
     commande = demon.RunCommand.End(bldr)
     bldr.Finish(commande)
@@ -61,9 +120,6 @@ def serialisation(path, args, envp, flags, stack_size):
     buf = bldr.Output()
 
     #Pour tester l'objet envoyé
-    test = demon.RunCommand.RunCommand.GetRootAs(buf, 0)
-    print(test.Path())
-    print(test.Envp(0))
+    #test = demon.RunCommand.RunCommand.GetRootAs(buf, 0)
 
 
-serialisation("echo", "test bonjour", "oui non", 0, 0)
