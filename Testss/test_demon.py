9044c143dab1a0878e53c64cec022f17960582c0
Fieux Telmo <xellio127@gmail.com>
2025-05-14 22:31:40 +0200

    Bug dans les tests

--- Testss/test_demon.py
+++ Testss/test_demon.py
@@ -22,7 +22,7 @@ def daemon(request):
 
     if daemon_type == "rust":
         process = subprocess.Popen(
-            ["cargo", "run", "--bin", "run_demon"],
+            ["cargo", "run", "--bin", "run_demon", "--", str(port)],
             cwd=RUST_PROJECT_DIR,
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
@@ -59,8 +59,10 @@ def connect_to_daemon(IP_address, daemon):
         print(f"Connexion vers {IP_address}:{port} réussie.")
         size_bytes = client.recv(4)
         size = int.from_bytes(size_bytes, byteorder='little')
+        print(f"size : {size}")
         data = client.recv(int(size))
         res_port = receive_TCPSocketListening(data,int(size))
+        print(f"received port {res_port}")
         if res_port == port :
             client.close()
             return 1
@@ -74,11 +76,125 @@ def connect_to_daemon(IP_address, daemon):
     except socket.error as e:
         print(f"Erreur de socket : {e}")
         return -1
+
+
+def launch_process(IP_address, daemon, command):
+    process, daemon_type, port = daemon
+    assert process.poll() is None
+    print(f"Testing connection for {daemon_type} daemon on port {port}")
+
+    try:
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client.connect((IP_address, port))
+        buf=send_command_to_demon(command)
+        header = buf.size.to_bytes(4, byteorder='little')
+        client.sendall(header + buf.buffer)
+
+        size_bytes= client.recv(4)
+        size = int.from_bytes(size_bytes, byteorder='little')
+        data = client.recv(int(size))
+        pid = receive_processlaunched(data,int(size))
+        print(f"pid : {pid}")
+        if pid < 0 :
+            client.close()
+            return -1
+        else :
+            client.close()
+            return 1
+    except ConnectionRefusedError:
+        print(f"Échec de la connexion : Le serveur à {IP_address}:{port} a refusé la connexion.")
+        print(f"Assurez-vous que le serveur {daemon_type} est lancé et écoute sur le bon port.")
+        return -1
+    except socket.error as e:
+        print(f"Erreur de socket : {e}")
+        return -1
     
-@pytest.mark.timeout(3)  # Timeout de 5 secondes
+def fail_launch_process(IP_address, daemon, command):
+    process, daemon_type, port = daemon
+    assert process.poll() is None
+    print(f"Testing connection for {daemon_type} daemon on port {port}")
+
+    try:
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client.connect((IP_address, port))
+        buf=send_command_to_demon(command)
+        header = buf.size.to_bytes(4, byteorder='little')
+        client.sendall(header + buf.buffer)
+
+        size_bytes= client.recv(4)
+        size = int.from_bytes(size_bytes, byteorder='little')
+        data = client.recv(int(size))
+        error_code = receive_childcreationerror(data,int(size))
+        print(f"error_code : {error_code}")
+        if error_code < 0 :
+            client.close()
+            return -1
+        else :
+            client.close()
+            return 1
+    except ConnectionRefusedError:
+        print(f"Échec de la connexion : Le serveur à {IP_address}:{port} a refusé la connexion.")
+        print(f"Assurez-vous que le serveur {daemon_type} est lancé et écoute sur le bon port.")
+        return -1
+    except socket.error as e:
+        print(f"Erreur de socket : {e}")
+        return -1
+    
+
+@pytest.mark.timeout(3)
 def test_daemon_connection(daemon):
     res = connect_to_daemon("127.0.0.1", daemon)
     assert res != -1, "Connexion échouée"
+
+@pytest.mark.timeout(3)
+def test_launch_process(daemon):
+    # Configuration des arguments
+    path = "/bin/ls"
+    args = ["ls", "-l"]
+    envp = []
+    flags = 0
+    stack_size = 1024 * 1024  # 1 MB de stack
+
+    # Configuration des événements à surveiller
+    to_watch = []
+
+    # Création de la commande
+    command = Command(
+        path=path,
+        args=args,
+        envp=envp,
+        flags=flags,
+        stack_size=stack_size,
+        to_watch=to_watch,
+        to_watch_size=len(to_watch)
+    )
+    res = launch_process("127.0.0.1", daemon,command)
+    assert res != -1, "Launch process failed"
+
+@pytest.mark.timeout(3)
+def test_failed_launch_process(daemon):
+    # Configuration des arguments
+    path = "/bin/OIHDOEJ"
+    args = ["ls", "-l"]
+    envp = []
+    flags = 0
+    stack_size = 1024 * 1024  # 1 MB de stack
+
+    # Configuration des événements à surveiller
+    to_watch = []
+
+    # Création de la commande
+    command = Command(
+        path=path,
+        args=args,
+        envp=envp,
+        flags=flags,
+        stack_size=stack_size,
+        to_watch=to_watch,
+        to_watch_size=len(to_watch)
+    )
+    res = fail_launch_process("127.0.0.1", daemon,command)
+    assert res != -1, "Receiving fail info failed"
 
 
 
